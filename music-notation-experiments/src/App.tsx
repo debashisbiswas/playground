9b506e7c1995fc347d62baa8b4bc8b746302c206
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2026-03-14 21:41:00 -0700

    hello world notation

--- music-notation-experiments/src/App.tsx
+++ music-notation-experiments/src/App.tsx
@@ -1,9 +1,28 @@
+import { useEffect, useRef } from "react";
 import "./App.css";
+import opensheetmusicdisplay from "opensheetmusicdisplay";
+import sampleXML from "./assets/hello-world.musicxml?raw";
 
 function App() {
+  const containerId = "osmdContainer";
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const osmd = new opensheetmusicdisplay.OpenSheetMusicDisplay(containerId, {
+      autoResize: true,
+      backend: "svg",
+      drawingParameters: "compacttight",
+      autoBeam: true,
+    });
+
+    osmd.load(sampleXML).then(() => {
+      osmd.render();
+    });
+  });
+
   return (
     <>
-      <div>Hello world</div>
+      <div ref={containerRef} id={containerId}></div>
     </>
   );
 }
