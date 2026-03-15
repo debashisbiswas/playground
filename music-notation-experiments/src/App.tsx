1f983d7cfbee3b1978f03de98780f1d0013e4df4
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2026-03-14 21:47:00 -0700

    generate music xml

--- music-notation-experiments/src/App.tsx
+++ music-notation-experiments/src/App.tsx
@@ -1,7 +1,48 @@
 import { useEffect, useRef } from "react";
 import "./App.css";
 import opensheetmusicdisplay from "opensheetmusicdisplay";
-import sampleXML from "./assets/hello-world.musicxml?raw";
+
+const createMusicXML = () => {
+  return `
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!DOCTYPE score-partwise PUBLIC
+    "-//Recordare//DTD MusicXML 4.0 Partwise//EN"
+    "http://www.musicxml.org/dtds/partwise.dtd">
+<score-partwise version="4.0">
+  <part-list>
+    <score-part id="P1">
+      <part-name>Music</part-name>
+    </score-part>
+  </part-list>
+  <part id="P1">
+    <measure number="1">
+      <attributes>
+        <divisions>1</divisions>
+        <key>
+          <fifths>0</fifths>
+        </key>
+        <time>
+          <beats>4</beats>
+          <beat-type>4</beat-type>
+        </time>
+        <clef>
+          <sign>G</sign>
+          <line>2</line>
+        </clef>
+      </attributes>
+      <note>
+        <pitch>
+          <step>C</step>
+          <octave>4</octave>
+        </pitch>
+        <duration>4</duration>
+        <type>whole</type>
+      </note>
+    </measure>
+  </part>
+</score-partwise>
+`;
+};
 
 function App() {
   const containerId = "osmdContainer";
@@ -15,7 +56,9 @@ function App() {
       autoBeam: true,
     });
 
-    osmd.load(sampleXML).then(() => {
+    const notation = createMusicXML();
+
+    osmd.load(notation).then(() => {
       osmd.render();
     });
   });
